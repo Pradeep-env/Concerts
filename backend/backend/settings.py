e5826116428aaf74271c5f01ff9966da95200c55
Pradeep-env <pradeepkh312@gmail.com>
2026-09-15 15:23:04 +0530

    jwt config remove refresh token rotation, jwt urls added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -149,9 +149,9 @@ from datetime import timedelta
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
-    'ROTATE_REFRESH_TOKENS':True,
-    'BLACKLIST_AFTER_ROTATION':True,
-    'UPDATE_LAST_LOGIN': True,
+    # 'ROTATE_REFRESH_TOKENS':True,
+    # 'BLACKLIST_AFTER_ROTATION':True,
+    # 'UPDATE_LAST_LOGIN': True,
     'ALGORITHM': 'HS256',
     'SIGNING_KEY': os.environ.get("JWT_SECRET_KEY", "jwt_secret_key"),
     'AUTH_HEADER_TYPES': ('Bearer',),
